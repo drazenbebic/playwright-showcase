ad8df979f5f2700d9d0ef31a4bb16e27931cb17e
Drilon Buzuku <drilon.buku@gmail.com>
2024-04-23 00:14:40 +0200

    feat: Add GH prime & console log zustand state data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { ThemeProvider, BaseStyles } from '@primer/react';
 import AddParticipantForm from './components/AddParticipant/AddParticipantForm'
+import useStore from './store';
 import './App.css'
 
 function App() {
+  const data = useStore((state: any) => state.data);
+  console.log(data, 'zustand');
+
   return (
-    <div>
-      <AddParticipantForm />
-    </div>
-  )
+    <ThemeProvider>
+      <BaseStyles>
+        <div>
+          <AddParticipantForm />
+        </div>
+      </BaseStyles>
+    </ThemeProvider>
+  );
 }
 
 export default App
