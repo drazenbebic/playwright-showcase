d782a00166e2cc6a2a22e5b73ea3d720ce910e99
Drazen Bebic <drazen.bebic@outlook.com>
2024-04-23 13:33:44 +0200

    fix: fix form issues

--- src/components/AddParticipant/AddParticipantForm.tsx
+++ src/components/AddParticipant/AddParticipantForm.tsx
@@ -2,7 +2,6 @@ import useStore, { Participant, StoreState } from '../../store';
 import * as Ariakit from '@ariakit/react';
 import { Button, FormControl, TextInput, Heading } from '@primer/react'
 import classNames from 'classnames';
-
 import styles from './AddParticipantForm.module.scss';
 
 const AddParticipantForm = () => {
@@ -11,10 +10,19 @@ const AddParticipantForm = () => {
   const participantsList = useStore((state: StoreState) => state.data);
   const clearAllParticipants = useStore((state: StoreState) => state.clearData);
 
+  form.useValidate(async ({ values: { name, email } }) => {
+    if (!name) {
+      form.setError(form.names.name, 'Please type in a name.');
+    }
+    if (!email) {
+      form.setError(form.names.email, 'Please type in an email.');
+    }
+  })
+
   form.useSubmit(async (state) => {
     const values = state.values;
     const { email, name } = values;
-    const participant: Participant = {id: `${participantsList.length + 1}`, email, name} 
+    const participant: Participant = {id: `${participantsList.length + 1}`, email, name}
 
     addObject(participant);
   });
@@ -30,8 +38,11 @@ const AddParticipantForm = () => {
           className="wrapper"
         >
           <div className={styles.formFieldsWrapper}>
-            <div className="field">
-              <FormControl.Label>Name</FormControl.Label>
+            <div className={styles.field}>
+              <Ariakit.FormLabel
+                name={form.names.name}
+                render={<FormControl.Label>Name</FormControl.Label>}
+              />
               <Ariakit.FormInput
                 render={<TextInput required />}
                 name={form.names.name}
@@ -39,25 +50,39 @@ const AddParticipantForm = () => {
                 className={styles.formField}
                 required
               />
-              <Ariakit.FormError name={form.names.name} className="error" />
+              <Ariakit.FormError name={form.names.name} className={styles.error} />
             </div>
-            <div className="field">
-              <FormControl.Label>E-Mail</FormControl.Label>
+            <div className={styles.field}>
+              <Ariakit.FormLabel
+                name={form.names.email}
+                render={<FormControl.Label>E-Mail</FormControl.Label>}
+              />
               <Ariakit.FormInput
                 render={<TextInput required />}
-                type="text"
+                type="email"
                 name={form.names.email}
                 placeholder="johndoe@example.com"
                 className={styles.formField}
                 required
               />
-              <Ariakit.FormError name={form.names.email} className="error" />
+              <Ariakit.FormError name={form.names.email} className={styles.error} />
             </div>
           </div>
           <div className={styles.actionsWrapper}>
-            <Button onClick={() => form.submit()} className={classNames(styles.button, styles.addParticipantButton)}>
-              Add
-            </Button>
+            <Ariakit.FormSubmit
+              store={form}
+              render={
+                <Button variant="primary" className={classNames(styles.button, styles.block)}>
+                  Add
+                </Button>
+              }
+            />
+            <Ariakit.FormReset
+              store={form}
+              render={
+                <Button className={classNames(styles.button)}>Reset</Button>
+              }
+            />
             <Button onClick={() => clearAllParticipants()} className={classNames(styles.button, styles.dangerButton)}>
               Reset all participants
             </Button>
